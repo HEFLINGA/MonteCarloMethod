927abf533a6a856dfd956530f88e6a969866b8a0
Auston Hefling <auston.hefling@outlook.com>
2018-09-10 15:29:35 -0400

    Finish Monte Carlo Method

--- MonteCarlo/Program.cs
+++ MonteCarlo/Program.cs
@@ -10,16 +10,20 @@ namespace MonteCarlo
     {
         static void Main(string[] args)
         {
+            // Start of array/ Prompt user for array size
             Console.WriteLine("Input array size");
             int input = int.Parse(Console.ReadLine());
+
+            // Initialize structs
             Random rnd = new Random();
             XY hyp = new XY();
             
-
+            // Initialize array/array length/counter
             double[,] arr = new double[input, 2];
             int len = arr.Length - input;
             int counter = 0;
 
+            // Loop for random X and Y coords in multiDem array
             for (int i = 0; i < len; ++i)
             {
                 XY rand = new XY(rnd);
@@ -28,18 +32,21 @@ namespace MonteCarlo
                 Console.WriteLine($"X: {rand.X()}.  Y: {rand.Y()}");                
                 Console.WriteLine($"Hyp: {hyp.Hypotenuse(rand.X(), rand.Y())}");
 
+                // Increments counter for hypotenuse inside of circle
                 if (hyp.Hypotenuse(rand.X(), rand.Y()) <= 1)
                 {
                     counter++;
                 }                
             }
 
+            // Step 5 of Monte Carlo
             double coord = 0;
             double length = len;
             double counter1 = counter;
             coord += counter1 / length;
             double coord1 = coord * 4;
 
+            // Writes all results of counter, result, and difference from pi
             Console.WriteLine($"Counter: {counter}");
             Console.WriteLine($"Result: {coord1}");
             double diff = Math.Abs(coord1 - Math.PI);
