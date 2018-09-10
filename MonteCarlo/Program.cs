59323df681614628ac2d1ca421ddfcf6f278fbe9
Auston Hefling <auston.hefling@outlook.com>
2018-09-10 14:39:57 -0400

    Add code to make method work

--- MonteCarlo/Program.cs
+++ MonteCarlo/Program.cs
@@ -10,12 +10,42 @@ namespace MonteCarlo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number:");
-
+            Console.WriteLine("Input array size");
+            int input = int.Parse(Console.ReadLine());
+            XY hyp = new XY();
             Random rnd = new Random();
 
-            int size = int.Parse(Console.ReadLine());
-            int[] arrays = new int[size];
+            double[,] arr = new double[input, 2];
+            int len = arr.Length - input;
+            int counter = 0;
+
+            for (int i = 0; i < len; ++i)
+            {
+                XY rand = new XY(rnd);
+                arr[i, 0] = rand.X();
+                arr[i, 1] = rand.Y();
+                Console.WriteLine($"X: {rand.X()}.  Y: {rand.Y()}");                
+                Console.WriteLine($"Hyp: {hyp.Hypotenuse(rand.X(), rand.Y())}");
+
+                if (hyp.Hypotenuse(rand.X(), rand.Y()) <= 1)
+                {
+                    counter++;
+                }                
+            }
+
+            double coord = 0;
+            double length = len;
+            double counter1 = counter;
+            coord += counter1 / length;
+            double coord1 = coord * 4;
+
+            Console.WriteLine($"Counter: {counter}");
+            Console.WriteLine($"Result: {coord1}");
+            double diff = Math.Abs(coord1 - Math.PI);
+            Console.WriteLine($"Difference: {diff}");
+
+            Console.WriteLine($" ");
+
 
         }
     }
--- MonteCarlo/Struct.cs
+++ MonteCarlo/Struct.cs
@@ -6,26 +6,35 @@ using System.Threading.Tasks;
 
 namespace MonteCarlo
 {
-    struct Coordinate
+    public struct XY
     {
-        public double x;
-        public double y;
+        public double x, y;
 
-        public Coordinate(double x, double y)
+        public XY(double x, double y)
         {
             this.x = x;
             this.y = y;
         }
 
-        public Coordinate(Random obj)
+        public XY(Random obj)
         {
             x = obj.NextDouble();
             y = obj.NextDouble();
         }
 
+        public double X()
+        {
+            return this.x;
+        }
+
+        public double Y()
+        {
+            return this.y;
+        }
+
         public double Hypotenuse(double x, double y)
         {
-            double hypotenuse = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            double hypotenuse = Math.Sqrt((Math.Pow(x, 2)) + (Math.Pow(y, 2)));
 
             return hypotenuse;
         }
